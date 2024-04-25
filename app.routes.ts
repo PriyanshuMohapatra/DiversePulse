d2e8035fe11998590f9066bf258a4cb8d9acb729
Priyanshu Mohapatra <119274090+PriyanshuMohapatra@users.noreply.github.com>
2024-04-25 17:38:31 +0530

    Delete app.routes.ts

--- app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { FstComponent } from './fst/fst.component';
-
-export const routes: Routes = [
-    {path : 'fst',component:FstComponent}
-    
-];
