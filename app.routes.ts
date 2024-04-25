bab12513613c99e27368b2456ebec893fccca0b3
Priyanshu Mohapatra <119274090+PriyanshuMohapatra@users.noreply.github.com>
2024-04-25 17:31:15 +0530

    Add files via upload

---
+++ app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { FstComponent } from './fst/fst.component';
+
+export const routes: Routes = [
+    {path : 'fst',component:FstComponent}
+    
+];
